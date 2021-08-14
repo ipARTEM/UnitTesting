884b4b4614b3e7627dc95d91ddd87ff907db6c30
Artem <a.khimin@yandex.ru>
2021-08-14 23:01:22 +0300

    тест Exceptions

---
+++ AssertSamples.Tests2/ExpectingExceptions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssertSamples.Tests2
+{
+    [TestClass]
+    public  class ExpectingExceptions
+    {
+
+        [ExpectedException(typeof(ArgumentNullException),"Exception was not thrown")]
+        [TestMethod]
+        public void MyClass_SayHello_Exception()
+        {
+            MyClass instance = new MyClass();
+            instance.SayHello(null);
+        }
+
+        [TestMethod]
+        public void MyClass_SayHello_ReturnArtem()
+        {
+            //arrange
+            MyClass instance = new MyClass();
+            string expected = "Hello Artem";
+
+            //act
+            string actual = instance.SayHello("Artem");
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+
+    }
+}
---
+++ AssertSamples.Tests2/StringAssertMethods.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AssertSamples.Tests2
+{
+    [TestClass]
+    public  class StringAssertMethods
+    {
+        [TestMethod]
+        public void StringContainsTest()
+        {
+            // проверка на вхождение в строку подстроки
+            StringAssert.Contains("Assert samples", "sam");
+        }
+
+        [TestMethod]
+        public void StringMatchesTest()
+        {
+
+            StringAssert.Matches("123", new Regex(@"\d{3}"));
+        }
+
+        [TestMethod]
+        public void StringStartdWithTest()
+        {
+            StringAssert.StartsWith("Hello planet", "Hello");
+        }
+
+        public void StringEnddWithTest()
+        {
+            StringAssert.EndsWith("Hello planet", "planet");
+        }
+
+
+
+    }
+}
